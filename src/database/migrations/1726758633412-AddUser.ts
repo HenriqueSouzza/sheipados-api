7717526457720cf04dbe873c91a66ff69ef35a2f
Henrique Souza <henriquetsi@gmail.com>
2024-09-19 16:58:57 -0300

    feat: add migration

--- src/database/migrations/1726758633412-AddUser.ts
+++ src/database/migrations/1726758633412-AddUser.ts
@@ -8,7 +8,7 @@ export class AddUser1726758633412 implements MigrationInterface {
             queryRunner.manager.create(User, {
                 email: 'henriquetsi@gmail.com',
                 name: 'Henrique Souza',
-                password: '1597bdd3b2fb5b7e478b80b073b5d6cc094f4471566388f9899b9f4317c04709',
+                password: process.env.PASSWORD_DEFAULT,
                 username: 'henriquetsi',
                 isActive: true,
                 firstLogin: true,
