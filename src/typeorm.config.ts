baa373f28cc71c3d5a1f7dc88c6435c05960115e
Henrique Pereira De Souza <henriquesouza@CAJU-MAC-0140.local>
2024-10-22 10:07:54 -0300

    feat: remove rule env dev and add nvmrc

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -2,11 +2,9 @@ import 'dotenv/config'
 import { join } from 'path';
 import { DataSource, DataSourceOptions } from "typeorm";
 
-const isDev = process.env.ENVIRONMENT === 'dev';
-
 export const config: DataSourceOptions = ({
   type: 'mysql',
-  host: isDev ? '172.17.0.1' : process.env.DATABASE_HOST,
+  host: process.env.DATABASE_HOST,
   port: parseInt(process.env.DATABASE_PORT),
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
